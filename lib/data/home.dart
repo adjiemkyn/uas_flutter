22d2269843423e885bb1f05752449fe897fed0e9
adjiemkyn <adjiemikayana@gmail.com>
2024-04-08 20:22:21 +0800

    home

---
+++ lib/data/home.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:project_uts/data/list_tempat.dart';
+
+class home extends StatelessWidget {
+  const home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Daftar Destinasi Wisata'),
+      ),
+      body: ListView.builder(
+          itemCount: dataWisata.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => DetailScreen(
+                              reseps: dataWisata[index],
+                            )));
+              },
+              child: Card(
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(30),
+                        child: Image.asset(
+                          dataWisata[index].image,
+                          width: 150,
+                          height: 150,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(dataWisata[index].name,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25,
+                                color: Colors.blueAccent)),
+                        Text('HTM : ${dataWisata[index].htm}'),
+                        ClipOval(
+                          child: Material(
+                            color: Colors.blue, // button color
+                            child: InkWell(
+                              splashColor: Colors.red, // inkwell color
+                              child: SizedBox(
+                                  width: 30,
+                                  height: 30,
+                                  child: Icon(Icons.favorite)),
+                              onTap: () {},
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            );
+          }),
+    );
+  }
+}
